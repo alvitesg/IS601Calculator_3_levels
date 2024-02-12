6509eb8da9c6f530ad3558cde9a41524f757ac66
alvitesg <ga329@njit.edu>
2024-02-12 17:08:29 -0500

    removed spaces from lines to address some of the pytint cov failed results

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -8,20 +8,20 @@ class Calculator:
     @staticmethod
     def add(a,b):
         """Add numbers and return value"""
-        calculation = Calculation(a, b, add)  
+        calculation = Calculation(a, b, add)
         return calculation.get_result()
     @staticmethod
     def subtract(a,b):
         """Subtract numbers and return value"""
-        calculation = Calculation(a, b, subtract)  
+        calculation = Calculation(a, b, subtract)
         return calculation.get_result()
     @staticmethod
     def multiply(a,b):
         """Multiply numbers and return value"""
-        calculation = Calculation(a, b, multiply)  
+        calculation = Calculation(a, b, multiply)
         return calculation.get_result()
     @staticmethod
     def divide(a,b):
         """Divide numbers and return value"""
-        calculation = Calculation(a, b, divide)  
+        calculation = Calculation(a, b, divide)
         return calculation.get_result()
