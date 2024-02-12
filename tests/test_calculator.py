301bf6e6a21ec144581ffda3af8bcabf01ae0ea2
alvitesg <ga329@njit.edu>
2024-02-12 01:23:54 -0500

    added tests to run on my calculator

---
+++ tests/test_calculator.py
@@ -0,0 +1,18 @@
+'''My calculator test'''
+from calculator import add, substract, multiply, divide
+
+def test_addition():
+    '''Test that addition function works'''
+    assert add(2,2) == 4
+
+def test_subtraction():
+    '''Test that subtraction function works'''
+    assert substract(2,2) == 0
+
+def test_multiply():
+    '''Test that multiply function works'''
+    assert multiply(2,2) == 4
+
+def test_divide():
+    '''Test that division function works'''
+    assert divide(2,2) == 1
